1256aac0c49af288b57d33294dcea762dbbe1159
adityavk1 <adityavka@yahoo.com>
2016-05-17 18:38:07 -0400

    Modified Tutorial PHP Files
    I modified the tutorial PHP files since the original file gave me a HTTP
    405 Error.

--- process.php
+++ process.php
@@ -3,6 +3,6 @@ Thanks, <?php
  $sub="Form feedback";
  $name = stripslashes($name);
  $email = stripslashes($email);
- mail('admin@your-domain.com',$sub,"$name $email","From: $name <admin@your-domain.com>");
+ mail('adityavka@yahoo.com',$sub,"$name $email","From: $name <adityavka@yahoo.com>");
  echo stripslashes($name);
  ?> , we will drop you a line shortly. 
