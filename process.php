da8ecf4c63e4cc535bfd3c8c9fb833190556721e
adityavk1 <adityavka@yahoo.com>
2016-05-20 23:05:57 -0400

    Website Maintenance
    Test files that were initially uploaded have been removed.

--- process.php
+++
@@ -1,8 +0,0 @@
-Thanks, <?php
- @extract($_POST);
- $sub="Form feedback";
- $name = stripslashes($name);
- $email = stripslashes($email);
- mail('adityavka@yahoo.com',$sub,"$name $email","From: $name <adityavka@yahoo.com>");
- echo stripslashes($name);
- ?> , we will drop you a line shortly. 
